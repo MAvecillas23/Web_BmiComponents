96e90df6ff2b0886ddbf9db689f09578c1abe78f
Michael <email@>
2024-11-11 19:16:21 -0600

    Finished Lab

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import BodyMassIndexForm from "./components/BodyMassIndexForm.vue";
+import { ref } from 'vue'
+
+// after calculations bmi will be stored here
+const bmi = ref(0)
+
+// function that calculates emitted height and weight from BodyMassIndexForm.vue
+// to get bmi and assign that value to the bmi variable
+function calculateBmi(height, weight){
+  bmi.value = weight / (height * height)
+
+}
+
+</script>
+
+<template>
+  <div id="app-component">
+    <h1>Body Mass Index Calculator</h1>
+<!--    get emitted data from BodyMassIndexForm using v-on and then calling the calculateBmi function-->
+    <BodyMassIndexForm v-on:stats-entered="calculateBmi"></BodyMassIndexForm>
+
+<!--bmi rounded to two decimal places-->
+    <p> Your BMI is {{ bmi.toFixed(2) }}</p>
+  </div>
+</template>
+
+<style scoped>
+#app-component {
+  border: blue 5px solid;
+  padding: 40px
+}
+
+</style>
---
+++ src/components/BodyMassIndexForm.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+// inputted height and weight is stored here
+const userHeight = ref()
+const userWeight = ref()
+
+// this is used to be able to send arguments to App.vue
+const emit = defineEmits(['stats-entered'])
+
+// function to run when user clicks the button
+// this function will emit a message to App.vue including
+// the user's height and weight data
+function sendBmi() {
+  emit('stats-entered', userHeight.value, userWeight.value)
+}
+</script>
+
+<template>
+  <div id="bmif">
+<!--    v-models for input height and weight, data is stored in their respective variables -->
+    <h2>Enter your height and weight</h2>
+    <label>Height in meters: </label><input v-model="userHeight">
+    <br>
+    <label>Weight in kilograms: </label><input v-model="userWeight">
+    <br>
+  <!--  v-on to tell the script when the user clicks the button -->
+    <button v-on:click="sendBmi">Calculate</button>
+  </div>
+</template>
+
+<style scoped>
+#bmif {
+  border: red 5px solid;
+  padding: 20px
+}
+
+button {
+  border: black 1px solid;
+  margin: 10px;
+}
+</style>
